b89ea21770753dcb441395ad10eee1114fdf5f4c
TactialPotato <tristanmartinez@live.com>
2020-02-03 11:25:51 +0100

    Revert "Merge branch 'master' of https://github.com/SimonAspegren1/PV-G25-Game"
    This reverts commit 6815aeb7c9be4bda4258a22dbb0f55910c8c9b34, reversing
    changes made to fbfe8790164db623edb81ce6dad4ac7dcbb945d5.

--- HorrorGame/Assets/Script/InventoryClass.cs
+++ HorrorGame/Assets/Script/InventoryClass.cs
@@ -2,27 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class IventoryObjects : ScriptableObject
-//{
-//    //public List<ItemClass> Container = new List<ItemClass>();
+public class IventoryObjects : ScriptableObject
+{
+    public List<ItemClass> Container = new List<ItemClass>();
 
-//    [System.Serializable]
+    [System.Serializable]
 
-//    public class InventorySlot
-//    {
-//        public ItemClass myItem;
-//        public int myAmount;
-//        public InventorySlot(ItemClass _myItem, int _myAmount)
-//        {
-//            myItem = _myItem;
-//            myAmount = _myAmount;
-//        }
-//        public void AddAmount(int Value)
-//        {
+    public class InventorySlot
+    {
+        public ItemClass myItem;
+        public int myAmount;
+        public InventorySlot(ItemClass _myItem, int _myAmount)
+        {
+            myItem = _myItem;
+            myAmount = _myAmount;
+        }
+        public void AddAmount(int Value)
+        {
 
-//        }
+        }
 
-//    }
+    }
 
 
-//}
+}
