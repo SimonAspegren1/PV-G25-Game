b07675f2204328a2b4e4f04cd77a297a3f9426d4
weird-guitar <rayyan.sarwar@hotmail.com>
2020-02-03 10:50:56 +0100

    Sound and Soundscript
    lägger till ett halv färdigt ljudscript och ljud mapp.

---
+++ HorrorGame/Assets/Script/Soundmanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundmanager : MonoBehaviour
+{
+    public static Audioclip
+    static Audiosource audioSrce; 
+
+    void start() {
+
+        ButtonSound =
+        BackgroundMusic =
+        ChaseMusic =
+        Enemyfootsteps =
+        Plyerfootsteps =
+        Ambience =
+
+
+        audiosrce = GetComponent<audiosource> (); 
+}
+
+    void Update() {
+
+}   
+    public static void Playsound (string clip)
+    {
+         switch (Clip) { 
+         case " violin ":
+         audioSrce.PlayOneShot(violin);
+         break; 
+         
+
+
+
+    }
+
+
+ 
+
+
