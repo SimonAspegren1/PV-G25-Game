1256b287525f92fe6ff88a3d584a782a9afdfb56
GG <48587537+Hoirshou@users.noreply.github.com>
2020-02-04 10:56:20 +0100

    Revert "Inventory"
    This reverts commit 9812859b0dd4e9f12aff5008b7776cb78c676153.

--- HorrorGame/Assets/Script/Inventory/InventoryClass.cs
+++ HorrorGame/Assets/Script/Inventory/InventoryClass.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
-public class InventoryObject : ScriptableObject
+public class InventoryClass : ScriptableObject
 {
     public List<InventorySlot> Container = new List<InventorySlot>();
-    public void AddItem(ItemObject _myItem, int _myAmount)
+    public void AddItem(ItemClass _myItem, int _myAmount)
     {
         bool tempHasItem = false;
         for (int i = 0; i < Container.Count; i++)
@@ -26,9 +26,9 @@ public class InventoryObject : ScriptableObject
     [System.Serializable]
     public class InventorySlot
     {
-        public ItemObject myItem;
+        public ItemClass myItem;
         public int myAmount;
-        public InventorySlot(ItemObject _myItem, int _myAmount)
+        public InventorySlot(ItemClass _myItem, int _myAmount)
         {
             myItem = _myItem;
             myAmount = _myAmount;
--- HorrorGame/Assets/Script/Inventory/TestItem.cs
+++ HorrorGame/Assets/Script/Inventory/TestItem.cs
@@ -2,30 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class TestItem : TestScript
-//{
-//    [SerializeField] string Name, Description;
-//    [SerializeField] float AmountToRestore;
-//    Player Player;
-//    FloatValue PlayerHpFloatValue;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-//        ItemName = Name;
-//        ItemDescription = Description;
-//        PlayerHpFloatValue = Player.currentHealth;
-//        PlayerHpFloatValue.RuntimeValue -= 3;
-//    }
+public class TestItem : ItemClass
+{
+    [SerializeField] string Name, Description;
+    [SerializeField] float AmountToRestore;
+    Player Player;
+    FloatValue PlayerHpFloatValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        ItemName = Name;
+        ItemDescription = Description;
+        PlayerHpFloatValue = Player.currentHealth;
+        PlayerHpFloatValue.RuntimeValue -= 3;
+    }
 
-   
+    // Update is called once per frame
+    //public void Awake ()
+    //{
+    //    type = ItemType.Medkit;
+    //}
 
-//    public override void UseItem()
-//    {
-//        PlayerHpFloatValue.RuntimeValue += AmountToRestore;
-//        if (PlayerHpFloatValue.RuntimeValue > PlayerHpFloatValue.initialValue)
-//        {
-//            PlayerHpFloatValue.RuntimeValue = PlayerHpFloatValue.initialValue;
-//        }
-//    }
-//}
+    public override void UseItem()
+    {
+        PlayerHpFloatValue.RuntimeValue += AmountToRestore;
+        if (PlayerHpFloatValue.RuntimeValue > PlayerHpFloatValue.initialValue)
+        {
+            PlayerHpFloatValue.RuntimeValue = PlayerHpFloatValue.initialValue;
+        }
+    }
+}
