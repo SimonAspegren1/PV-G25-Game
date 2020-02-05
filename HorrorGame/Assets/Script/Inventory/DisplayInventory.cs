e41db3e9afa86e05cff68b23a9966bec5eafd191
GG <48587537+Hoirshou@users.noreply.github.com>
2020-02-05 09:11:11 +0100

    Inventory

---
+++ HorrorGame/Assets/Script/Inventory/DisplayInventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventoryObjects Inventory;
+
+    public int mySpaceBetweenItemsX;
+    public int mySpaceBetweenItemsY;
+    public int myNumberOfColumns;
+
+    //Dictionary<InventorySlots, GameObject> itemsDisplayed = new Dictionary<InventorySlots, GameObject>;
+}
