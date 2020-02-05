0709728e01431e5492f26ec6646787708d12760b
GG <48587537+Hoirshou@users.noreply.github.com>
2020-02-05 10:47:25 +0100

    INVENTORY UI

--- HorrorGame/Assets/Script/Inventory/DisplayInventory.cs
+++ HorrorGame/Assets/Script/Inventory/DisplayInventory.cs
@@ -9,6 +9,9 @@ public class DisplayInventory : MonoBehaviour
     public int myXSpaceBetweenItem;
     public int myYSpaceBetweenItem;
     public int myNumberOfColumns;
+    public int myXstart;
+    public int myYStart;
+
 
     Dictionary<InventoryObjects.InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventoryObjects.InventorySlot, GameObject>();
     void Start()
@@ -17,7 +20,25 @@ public class DisplayInventory : MonoBehaviour
     }
     private void Update()
     {
-        //UpdateDisplay();
+        UpdateDisplay();
+    }
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].myAmount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].myItem.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPostion(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].myAmount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+            
+        }
     }
     public void CreateDisplay()
     {
@@ -26,11 +47,12 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].myItem.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPostion(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].myAmount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
     public Vector3 GetPostion(int i)
     {
-        return new Vector3(myXSpaceBetweenItem * (i % myNumberOfColumns), (-myYSpaceBetweenItem* (i/myNumberOfColumns)), 0f);
+        return new Vector3(myXstart + (myXSpaceBetweenItem * (i % myNumberOfColumns)), myYStart + (-myYSpaceBetweenItem* (i/myNumberOfColumns)), 0f);
     }
 
 }
