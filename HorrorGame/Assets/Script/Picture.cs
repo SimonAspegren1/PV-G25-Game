1812225a472583e055a9f2ae043d7006f49c8a6f
Alexander935 <47525933+Alexander935@users.noreply.github.com>
2020-02-05 11:03:50 +0100

    Picture

---
+++ HorrorGame/Assets/Script/Picture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Picture : ItemClass
+{
+    [SerializeField] string Name, Description;
+    [SerializeField] GameObject Panel;
+    [SerializeField] Button Exitbtn;
+    [SerializeField] ImageZoom IZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemName = Name;
+        ItemDescription = Description;
+        Exitbtn.onClick.AddListener(ExitPicture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void UseItem()
+    {
+        Panel.SetActive(true);
+    }
+
+    void ExitPicture()
+    {
+        IZ.currentZoom = 1;
+        IZ.content.position = IZ.StartPos;
+        Panel.SetActive(false);
+    }
+}
