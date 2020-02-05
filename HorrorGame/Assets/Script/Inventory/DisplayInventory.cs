2629833d5d73326736613406614159cf0a8df0a6
GG <48587537+Hoirshou@users.noreply.github.com>
2020-02-05 09:50:21 +0100

    Inv

--- HorrorGame/Assets/Script/Inventory/DisplayInventory.cs
+++ HorrorGame/Assets/Script/Inventory/DisplayInventory.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class DisplayInventory : MonoBehaviour
 {
     public InventoryObjects inventory;
@@ -10,5 +10,27 @@ public class DisplayInventory : MonoBehaviour
     public int myYSpaceBetweenItem;
     public int myNumberOfColumns;
 
-    //Dictionary<InventorySlots, GameObject> itemsDisplayed = new Dictionary<InventorySlots, GameObject>:
+    Dictionary<InventoryObjects.InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventoryObjects.InventorySlot, GameObject>();
+    void Start()
+    {
+        CreateDisplay();
+    }
+    private void Update()
+    {
+        //UpdateDisplay();
+    }
+    public void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].myItem.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPostion(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].myAmount.ToString("n0");
+        }
+    }
+    public Vector3 GetPostion(int i)
+    {
+        return new Vector3(myXSpaceBetweenItem * (i % myNumberOfColumns), (-myYSpaceBetweenItem* (i/myNumberOfColumns)), 0f);
+    }
+
 }
