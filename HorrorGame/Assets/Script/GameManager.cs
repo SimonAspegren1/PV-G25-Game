6dbfae2ed632165b463a29479ad1e78d5493e122
weird-guitar <rayyan.sarwar@hotmail.com>
2020-02-07 10:54:20 +0100

    Night (Preffab)
    NIIGH

---
+++ HorrorGame/Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public void Endgame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Restart();
+        }
+    }
+
+    void Restart ()
+    {
+        SceneManager.Loadscene(SceneManager.GetActiveScene().name);
+    }
+}
