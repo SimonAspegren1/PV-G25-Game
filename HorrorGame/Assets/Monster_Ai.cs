9c6e5f1a1c2ed9600b1a6457856b556c6a9c92b6
Nigeboi <60090644+Nigeboi@users.noreply.github.com>
2020-02-07 10:41:59 +0100

    Just gonna upload this now

---
+++ HorrorGame/Assets/Monster_Ai.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_Ai : MonoBehaviour
+{
+    public Transform myTarget;
+    public float mySpeed = 200f;
+    public float nextWayPointDistance = 3f;
+}
