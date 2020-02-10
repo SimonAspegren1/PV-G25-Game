4130ada703a7581eb6124f28d1c2156b88c65e5c
Alexander935 <47525933+Alexander935@users.noreply.github.com>
2020-02-10 09:46:46 +0100

    La tillbaka circle

---
+++ HorrorGame/Assets/Script/ShowTextFOrTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextFOrTime : MonoBehaviour
+{
+    bool WriteText = false;
+    bool CanWriteText = true;
+    [SerializeField] float TimeToShowText;
+    float CurrentTime;
+    [SerializeField] Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanWriteText)
+        {
+            if (WriteText)
+            {
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && CanWriteText)
+        {
+            WriteText = true;
+        }
+    }
+}
