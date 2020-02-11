35ebf57753bf37305c36877461bd08e1a17ccf05
Nigeboi <60090644+Nigeboi@users.noreply.github.com>
2020-02-11 11:20:10 +0100

    FlashLight prefab

--- HorrorGame/Assets/rotate.cs
+++ HorrorGame/Assets/rotate.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 rotatePog;
+    public float battery;
+    bool isOn;
+
+    private void Start()
     {
-        
+        battery = 190f;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isOn)
+            {
+                isOn = false;
+            }
+            else
+            {
+                isOn = true;
+            }
+        }
+
+
+        if (battery > 0f && isOn)
+        {
+            rotatePog = Vector3.zero;
+            rotatePog.x = Input.GetAxisRaw("Horizontal");
+            rotatePog.y = Input.GetAxisRaw("Vertical");
+            if (rotatePog.x > 0f || rotatePog.y > 0f || rotatePog.x < 0f || rotatePog.y < 0f)
+            {
+                Debug.Log("Enters rotaion");
+                if (rotatePog.x > 0)
+                {
+                    this.gameObject.transform.eulerAngles = new Vector3(
+                    gameObject.transform.eulerAngles.x,
+                    gameObject.transform.eulerAngles.y,
+                    90);
+                }
+                if (rotatePog.x < 0)
+                {
+                    this.gameObject.transform.eulerAngles = new Vector3(
+                    gameObject.transform.eulerAngles.x,
+                    gameObject.transform.eulerAngles.y,
+                    -90);
+                }
+                if (rotatePog.y > 0)
+                {
+                    this.gameObject.transform.eulerAngles = new Vector3(
+                    gameObject.transform.eulerAngles.x,
+                    gameObject.transform.eulerAngles.y,
+                    180);
+                }
+                else if (rotatePog.y < 0)
+                {
+                    this.gameObject.transform.eulerAngles = new Vector3(
+                    gameObject.transform.eulerAngles.x,
+                    gameObject.transform.eulerAngles.y,
+                    0);
+                }
+            }
+
+
+            gameObject.GetComponent<Renderer>().enabled = true;
+
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
         
     }
 }
