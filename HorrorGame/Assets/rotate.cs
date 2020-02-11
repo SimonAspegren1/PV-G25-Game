445228ed3abf8939a768e5180a5535c23b3fdb1d
Nigeboi <60090644+Nigeboi@users.noreply.github.com>
2020-02-11 15:24:29 +0100

    FlashLight sänks i styrka under en viss tid

--- HorrorGame/Assets/rotate.cs
+++ HorrorGame/Assets/rotate.cs
@@ -7,10 +7,13 @@ public class rotate : MonoBehaviour
     Vector3 rotatePog;
     public float battery;
     bool isOn;
+    
+    public Vector2 originalSize;
 
     private void Start()
     {
-        battery = 190f;
+        battery = 100f;
+        transform.localScale = originalSize;
     }
 
     void Update()
@@ -27,48 +30,49 @@ public class rotate : MonoBehaviour
             }
         }
 
+        rotatePog = Vector3.zero;
+        rotatePog.x = Input.GetAxisRaw("Horizontal");
+        rotatePog.y = Input.GetAxisRaw("Vertical");
+        if (rotatePog.x > 0f || rotatePog.y > 0f || rotatePog.x < 0f || rotatePog.y < 0f)
+        {
+
+            if (rotatePog.x > 0)
+            {
+                this.gameObject.transform.eulerAngles = new Vector3(
+                gameObject.transform.eulerAngles.x,
+                gameObject.transform.eulerAngles.y,
+                90);
+            }
+            if (rotatePog.x < 0)
+            {
+                this.gameObject.transform.eulerAngles = new Vector3(
+                gameObject.transform.eulerAngles.x,
+                gameObject.transform.eulerAngles.y,
+                -90);
+            }
+            if (rotatePog.y > 0)
+            {
+                this.gameObject.transform.eulerAngles = new Vector3(
+                gameObject.transform.eulerAngles.x,
+                gameObject.transform.eulerAngles.y,
+                180);
+            }
+            else if (rotatePog.y < 0)
+            {
+                this.gameObject.transform.eulerAngles = new Vector3(
+                gameObject.transform.eulerAngles.x,
+                gameObject.transform.eulerAngles.y,
+                0);
+            }
+        }
 
         if (battery > 0f && isOn)
         {
-            rotatePog = Vector3.zero;
-            rotatePog.x = Input.GetAxisRaw("Horizontal");
-            rotatePog.y = Input.GetAxisRaw("Vertical");
-            if (rotatePog.x > 0f || rotatePog.y > 0f || rotatePog.x < 0f || rotatePog.y < 0f)
-            {
-                Debug.Log("Enters rotaion");
-                if (rotatePog.x > 0)
-                {
-                    this.gameObject.transform.eulerAngles = new Vector3(
-                    gameObject.transform.eulerAngles.x,
-                    gameObject.transform.eulerAngles.y,
-                    90);
-                }
-                if (rotatePog.x < 0)
-                {
-                    this.gameObject.transform.eulerAngles = new Vector3(
-                    gameObject.transform.eulerAngles.x,
-                    gameObject.transform.eulerAngles.y,
-                    -90);
-                }
-                if (rotatePog.y > 0)
-                {
-                    this.gameObject.transform.eulerAngles = new Vector3(
-                    gameObject.transform.eulerAngles.x,
-                    gameObject.transform.eulerAngles.y,
-                    180);
-                }
-                else if (rotatePog.y < 0)
-                {
-                    this.gameObject.transform.eulerAngles = new Vector3(
-                    gameObject.transform.eulerAngles.x,
-                    gameObject.transform.eulerAngles.y,
-                    0);
-                }
-            }
-
-
             gameObject.GetComponent<Renderer>().enabled = true;
 
+            transform.localScale = new Vector2(originalSize.x * battery * 0.01f, originalSize.y * battery * 0.01f);
+
+            battery -= Time.deltaTime;
         }
         else
         {
